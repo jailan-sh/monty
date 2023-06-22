817f43464b1775c16ae3e50117427e4953ee00dc
jailan-sh <jilan.elsherbiny@gmail.com>
2023-06-22 13:57:01 -0700

    add

--- 4-add.c
+++ 4-add.c
@@ -10,29 +10,23 @@
 
 void add_ord(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp1 = NULL;
-	stack_t *tmp2 = NULL;
-	stack_t *ptr = *stack;
-	int sum = 0;
+        stack_t *ptr = *stack;
+        int sum = 0;
 	int count = 0;
 
-	while (ptr)
-	{
-		count++;
-		ptr = ptr->next;
-	}
-	if (count < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		free_monty();
-		exit(EXIT_FAILURE);
-	}
-	tmp1 = *stack;
-	tmp2 = (*stack)->next;
-	sum = tmp1->n;
-	sum += tmp2->n;
-	(*stack) = tmp2;
+        while (ptr)
+        {
+                count++;
+                ptr = ptr->next;
+        }
+        if (count < 2)
+        {
+                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+                free_monty();
+                exit(EXIT_FAILURE);
+        }
+	sum = (*stack)->n;
+	sum += (*stack)->next->n;
+	pop_ord(stack, line_number);
 	(*stack)->n = sum;
-	tmp1->next = NULL;
-	free(tmp1);
 }
