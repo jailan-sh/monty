140c67fa1f68f9050dc0dcb05e19596771a99f4d
jailan-sh <jilan.elsherbiny@gmail.com>
2023-06-25 09:31:55 -0700

    last

--- pstr.c
+++ pstr.c
@@ -10,36 +10,16 @@
 void pstr_ord(stack_t **stack, unsigned int line_number)
 {
 	stack_t *ptr = *stack;
-	char *str = NULL;
-	int count = 0;
 	int i;
 	(void) line_number;
 
 	while (ptr)
 	{
-		count++;
+		i = ptr->n;
+		if (i < 0 || i > 127)
+			break;
+		printf("%c", i);
 		ptr = ptr->next;
 	}
-
-	if (*stack == NULL)
-	{
-		printf("\n");
-		return;
-	}
-	str = malloc(count + 1);
-	if (str == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_monty();
-		exit(EXIT_FAILURE);
-	}
-
-	for (i = 0; *stack != NULL && (*stack)->n > 0 && (*stack)->n >= 127; i++)
-	{
-		str[i] = (char)(*stack)->n;
-		*stack = (*stack)->next;
-	}
-	str[i] = '\0';
-	printf("%s\n", str);
-	free(str);
+	printf("\n");
 }
